67f3472c2bf0b6a7a6508b4e6fbf442cdbf6285e
KinsagiYuirin <yuunapat.7482@gmail.com>
2026-02-06 16:27:29 +0700

    Move,Shoot,Spawn

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,14 +3,54 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed;
+    public InputAction moveAction;
+    public InputAction shootAction;
+    public float xRange = 10;
+    public GameObject foodPrefab;
+    
+
     private void Awake()
     {
-
+        moveAction = InputSystem.actions.FindAction("Move");
+        shootAction = InputSystem.actions.FindAction("Shoot");
     }
 
     // Update is called once per frame
     void Update()
     {
+        var hInput = moveAction.ReadValue<Vector2>().x;
+        transform.Translate(hInput * speed * Time.deltaTime * Vector3.right);
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange,
+                transform.position.y,
+                transform.position.z);
+        }
+        else if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange,
+                transform.position.y,
+                transform.position.z);
+        }
+        if (shootAction.triggered)
+        {
+            Instantiate(foodPrefab, transform.position, Quaternion.identity);
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        Vector3 left =  new Vector3(-xRange,
+                transform.position.y,
+                transform.position.z);
+        Vector3 right = new Vector3(xRange,
+                transform.position.y,
+                transform.position.z);
+        Gizmos.DrawLine(left, right);
 
+        Gizmos.color= Color.green;
+        Gizmos.DrawSphere (left, 0.5f);
+        Gizmos.DrawSphere(right, 0.5f);
     }
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,10 +1,25 @@
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public GameObject[] dogPrefeb;
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            float x = Random.Range(-10, 10);
+            int index = Random.Range(0, dogPrefeb.Length);
+            if (dogPrefeb[index] != null)
+            {
 
+                Instantiate(dogPrefeb[index], new Vector3(x, 0, 20), Quaternion.Euler(0, 180, 0));
+            }
+            else
+            {
+                Debug.LogWarning($"dog prefeb{index}is null");
+            }
+        } 
     }
 }
